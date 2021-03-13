ac4ff86549e97248751b723253e3206869b148ec
Jose Francisco Paredes <josw.paredes1998@gmail.com>
2021-03-13 16:48:08 -0600

    JWT Auth Added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 250);
+            $table->string('email', 255)->unique();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\MoviePosterController;
 
@@ -17,21 +18,30 @@ use App\Http\Controllers\MoviePosterController;
 |
 */
 
-Route::prefix('v1')->group(function () {
-    
-    Route::prefix('admin')->group(function () {
-        Route::get('/movies',                        [ MovieController::class, 'getAllMovies'        ]);
-        Route::get('/movies/{movie_id}',             [ MovieController::class, 'getMovieByID'        ]);
-        Route::post('/movies',                       [ MovieController::class, 'storeNewMovie'       ]);
-        Route::put('/movies/{movie_id}',             [ MovieController::class, 'updateMovie'         ]);
-        Route::put('/movies/availability/{movie_id}',[ MovieController::class, 'updateavailability'  ]);
-        Route::delete('/movies/{movie_id}',          [ MovieController::class, 'deleteMovie'         ]);
+Route::group([ 'prefix' => 'v1'], function ($router) {
 
-        Route::prefix('movies/posters')->group(function () {
-            Route::get('/{poster_id}',      [ MoviePosterController::class, 'getMoviePoster']);
-            Route::post('/',                [ MoviePosterController::class, 'storeMoviePoster']);
-            Route::delete('/{poster_id}',   [ MoviePosterController::class, 'deleteMoviePoster']);
-        });
+    Route::prefix('auth')->group(function () {
+        Route::post('/login',       [ AuthController::class, 'login'    ]);
+        Route::post('/register',    [ AuthController::class, 'register' ]);
+        Route::post('/logout',      [ AuthController::class, 'logout'   ]);
     });
 
+    Route::middleware(['api_jwt_auth'])->group(function () {
+        
+        Route::prefix('admin')->group(function () {
+            Route::get('/movies',                        [ MovieController::class, 'getAllMovies'        ]);
+            Route::get('/movies/{movie_id}',             [ MovieController::class, 'getMovieByID'        ]);
+            Route::post('/movies',                       [ MovieController::class, 'storeNewMovie'       ]);
+            Route::put('/movies/{movie_id}',             [ MovieController::class, 'updateMovie'         ]);
+            Route::put('/movies/availability/{movie_id}',[ MovieController::class, 'updateavailability'  ]);
+            Route::delete('/movies/{movie_id}',          [ MovieController::class, 'deleteMovie'         ]);
+    
+            Route::prefix('movies/posters')->group(function () {
+                Route::get('/{poster_id}',      [ MoviePosterController::class, 'getMoviePoster']);
+                Route::post('/',                [ MoviePosterController::class, 'storeMoviePoster']);
+                Route::delete('/{poster_id}',   [ MoviePosterController::class, 'deleteMoviePoster']);
+            });
+        });
+
+    });
 });
